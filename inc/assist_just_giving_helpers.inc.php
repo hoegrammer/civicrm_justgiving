09bad63892a0f32c7fe17df98f7dbfc4b374c17e
naomi <naomirosenberguk@gmail.com>
2015-12-24 15:26:04 +0100

    Initial commit

---
+++ inc/assist_just_giving_helpers.inc.php
@@ -0,0 +1,39 @@
+<?php
+
+class assistJustGivingHelpers {
+    public static function csvFileToArray($filename) {
+
+        // this prevents problems with different line endings
+        // see http://www.thisprogrammingthing.com/2012/oddity-with-fgetcsv/
+        ini_set('auto_detect_line_endings', true);
+
+        if ( !file_exists($filename) ) {
+            throw new Exception("File not found: $filename");
+        }
+
+        $fp = fopen($filename, "r");
+        if ( !$fp ) {
+            throw new Exception("Couldn't open file: $filename");
+        }  
+        $returnVal = array();
+        $header = null;
+
+        while(($row = fgetcsv($fp)) !== false){
+            if($header === null){
+                $header = $row;
+                continue;
+            }
+
+            $newRow = array();
+            for($i = 0; $i<count($row); $i++){
+                $newRow[$header[$i]] = $row[$i];
+            }
+
+            $returnVal[] = $newRow;
+        }
+
+        fclose($fp);
+
+        return $returnVal;
+    }
+}
