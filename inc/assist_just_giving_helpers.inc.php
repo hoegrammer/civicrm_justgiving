dbcb0b5f9a8783aef6dcb90eeb8e7129eddb6127
naomi <naomirosenberguk@gmail.com>
2016-01-21 11:57:02 +0000

    Added new function/check csvConfirmQuoted
    Resolves #12 Check CSV is sane.
    New helper function csvConfirmQuoted() added, checks that an input
    CSV file is quoted (actually just checks that the first character
    is a " which seems sufficient). Used this prior to the actual CSV
    import to prevent a duff import (a non-quoted CSV file will result
    in numerous erroneous 'anonymous' entries).

--- inc/assist_just_giving_helpers.inc.php
+++ inc/assist_just_giving_helpers.inc.php
@@ -5,6 +5,28 @@
 
 class assistJustGivingHelpers {
 
+    public static function csvConfirmQuoted($filename) {
+        // A quoted CSV should start with '"', so just check for that
+        // Initial part of function repeats code from csvFileToArray
+        ini_set('auto_detect_line_endings', true);
+
+        if ( !file_exists($filename) ) {
+            throw new Exception("File not found: $filename");
+        }
+
+        $fp = fopen($filename, "r");
+        if ( !$fp ) {
+            throw new Exception("Couldn't open file: $filename");
+        }
+        // Code above is same as csvFileToArray; now the csvCheckQuoted-specific code,
+        if (feof($fp) ) { fclose($fp); return False; }  // Null file (or single-line file?)
+        $first_line = fgets($fp);
+        fclose($fp);
+        if (strlen($first_line) == 0) { return False; } // Empty file
+        return $first_line[0] == '"';
+
+    }
+
     public static function csvFileToArray($filename) {
 
         // this prevents problems with different line endings
