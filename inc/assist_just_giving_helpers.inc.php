2268afe0f98b90b14f48e013dc7ee3f3d98bd45e
naomi <naomirosenberguk@gmail.com>
2016-01-28 11:36:09 +0000

    Refactored code for getting data from file
    Including checking that file exists, is quoted, etc

--- inc/assist_just_giving_helpers.inc.php
+++ inc/assist_just_giving_helpers.inc.php
@@ -5,60 +5,56 @@
 
 class assistJustGivingHelpers {
 
-    public static function csvConfirmQuoted($filename) {
-        // A quoted CSV should start with '"', so just check for that
-        // Initial part of function repeats code from csvFileToArray
-        ini_set('auto_detect_line_endings', true);
-
-        if ( !file_exists($filename) ) {
-            throw new Exception("File not found: $filename");
-        }
-
-        $fp = fopen($filename, "r");
-        if ( !$fp ) {
-            throw new Exception("Couldn't open file: $filename");
-        }
-        // Code above is same as csvFileToArray; now the csvCheckQuoted-specific code,
-        if (feof($fp) ) { fclose($fp); return False; }  // Null file (or single-line file?)
-        $first_line = fgets($fp);
-        fclose($fp);
-        if (strlen($first_line) == 0) { return False; } // Empty file
-        return $first_line[0] == '"';
-    }
-
-    public static function csvFileToArray($filename) {
-
+    
+    public static function extractDataFromQuotedCSVFile($filename) {
         // this prevents problems with different line endings
         // see http://www.thisprogrammingthing.com/2012/oddity-with-fgetcsv/
         ini_set('auto_detect_line_endings', true);
-
+ 
         if ( !file_exists($filename) ) {
             throw new Exception("File not found: $filename");
         }
-
         $fp = fopen($filename, "r");
         if ( !$fp ) {
             throw new Exception("Couldn't open file: $filename");
         }  
-        $returnVal = array();
-        $header = null;
-
-        while(($row = fgetcsv($fp)) !== false){
-            if($header === null){
-                $header = $row;
-                continue;
-            }
-
-            $newRow = array();
-            for($i = 0; $i<count($row); $i++){
-                $newRow[$header[$i]] = $row[$i];
-            }
-
-            $returnVal[] = $newRow;
+        if (!self::csvIsQuoted($fp)) {
+            fclose($fp);
+            throw new Exception("CSV is not quoted (or file is empty)");           
         }
-
+        $dataAsArray = self::csvFileToArray($fp);
         fclose($fp);
-
-        return $returnVal;
+        return $dataAsArray;
     }
-}
+ 
+     private static function csvIsQuoted($fp) {
+         /*if (feof($fp) ) { fclose($fp); throw new Exception("Empty/null file"); }*/  // Null file (or single-line file?)
+         $first_line = fgets($fp);
+         rewind($fp); // Leave $fp at start of file so subsquent use finds $fp at start of file
+         return $first_line && $first_line[0] == '"';
+     }
+ 
+     private static function csvFileToArray($fp) {
+ 
+         $returnVal = array();
+         $header = null;
+ 
+         while(($row = fgetcsv($fp)) !== false){
+             if($header === null){
+                 $header = $row;
+                 continue;
+             }
+ 
+             $newRow = array();
+             for($i = 0; $i<count($row); $i++){
+                 $newRow[$header[$i]] = $row[$i];
+             }
+ 
+             $returnVal[] = $newRow;
+         }
+ 
+         rewind($fp); // Leave $fp at start in case $fp subsequently used
+ 
+         return $returnVal;
+     }
+ }
