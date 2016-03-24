9626f5a5e566a8aeceb153905f7803948ad99b40
naomi <naomirosenberguk@gmail.com>
2016-03-24 14:36:09 +0000

    More work on config file for site-specific settings

---
+++ inc/config.php
@@ -0,0 +1,10 @@
+<?php
+
+define('ANONYMOUS_CIVI_ID', "1365");
+define('JUST_GIVING_ID_FIELD_NAME', "custom_1");
+define('EVENT_SPONSORSHIP_FIELD_NAME', 'custom_28');
+define('ELIGIBLE_FOR_GIFT_AID_FIELD_NAME', 'custom_154');
+define('INCOME_CONDITION_FIELD_NAME', 'custom_142');
+
+?>
+
