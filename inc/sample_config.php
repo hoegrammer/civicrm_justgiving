131118c04abc0758f9e5d488b5919d029f118d61
naomi <naomi@assistsheffield.org.uk>
2016-03-24 15:39:22 +0000

    Added sample_config.php
    Model for the actual config.php file to be created by the site owner

---
+++ inc/sample_config.php
@@ -0,0 +1,11 @@
+<?php
+// Enter the values corresponding to your CiviCRM installation,
+// then move/copy it to config.php in the same directory.
+define('ANONYMOUS_CIVI_ID', "1234");
+define('JUST_GIVING_ID_FIELD_NAME', "custom_123");
+define('EVENT_SPONSORSHIP_FIELD_NAME', 'custom_234');
+define('ELIGIBLE_FOR_GIFT_AID_FIELD_NAME', 'custom_345');
+define('INCOME_CONDITION_FIELD_NAME', 'custom_456');
+
+?>
+
