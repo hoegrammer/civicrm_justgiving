1f230961b54c5e5d4e0eb63a8ba79d09bd43881a
Naomi <hoegrammer@users.noreply.github.com>
2018-02-09 16:59:36 +0000

    Update sample_config.php

--- inc/sample_config.php
+++ inc/sample_config.php
@@ -1,8 +1,10 @@
 <?php
 // Enter the values corresponding to your CiviCRM installation,
 // then move/copy it to config.php in the same directory.
-define('ANONYMOUS_CIVI_ID', "1234");
-define('JUST_GIVING_ID_FIELD_NAME', "custom_123");
+define('ANONYMOUS_CIVI_ID', "1234"); // the contact id of the contact who will "own" all anonymous donations.
+define('JUST_GIVING_ID_FIELD_NAME', "custom_123"); // the custom field for storing Just Giving ID against contacts. This has to be in a multirecord fieldset
+
+// Assist-specific custom fields on donations.
 define('EVENT_SPONSORSHIP_FIELD_NAME', 'custom_234');
 define('ELIGIBLE_FOR_GIFT_AID_FIELD_NAME', 'custom_345');
 define('INCOME_CONDITION_FIELD_NAME', 'custom_456');
