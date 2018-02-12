e9ec321075d15ceae6ded4f53e3696720192b1d2
naomi <naomirosenberguk@gmail.com>
2018-02-12 10:48:53 +0000

    Sets subtype and group on new and updated contacts

--- inc/sample_config.php
+++ inc/sample_config.php
@@ -4,6 +4,7 @@
 define('ANONYMOUS_CIVI_ID', "1234"); // the contact id of the contact who will "own" all anonymous donations.
 define('JUST_GIVING_ID_FIELD_NAME', "custom_123"); // the custom field for storing Just Giving ID against contacts. This has to be in a multirecord fieldset
 define('CONTACT_SUB_TYPE', "Supporter"); // the sub-type to create when making new contacts
+define('GROUP_ID', 'supporters'); // id of group to put new contacts in.
 
 // Assist-specific custom fields on donations.
 define('EVENT_SPONSORSHIP_FIELD_NAME', 'custom_234');
